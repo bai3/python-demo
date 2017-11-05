9aa7b93a300c34eaeddc502fa97dc08638ff6844
bai3 <2442694004@qq.com>
2017-11-05 20:59:29 +0800

    bzx

--- spider/1.1.py
+++ spider/1.1.py
@@ -1,4 +1,9 @@
+# -*- coding: UTF-8 -*-
 import urllib2
 
-response = urllib2.urlopen("https://www.baidu.com/")
-print(response.read())
+
+user_agent = "'Mozilla/5.0 (compatible; MSIE 9.0; Windows NT6.1; Trident/5.0;"
+header = {'User-Agent': user_agent}
+res = urllib2.Request('http://39.108.84.239')
+response = urllib2.urlopen(res)
+print response.read()
